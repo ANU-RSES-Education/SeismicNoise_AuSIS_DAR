c77b1d56d2b49b2b9075e60e5a42adf2e9642af0
Louis Moresi <louis.moresi@anu.edu.au>
2020-05-18 20:13:53 +1000

    Update seismosocialdistancing_settings.py

--- notebooks/seismosocialdistancing_settings.py
+++ notebooks/seismosocialdistancing_settings.py
@@ -8,11 +8,11 @@ from obspy import UTCDateTime
 # Set these values to control the notebook behaviour
 
 # Make sure you take at least a full week (>=7 days) before the first "ban"
-start = UTCDateTime("2020-01-15")
+start = UTCDateTime("2020-12-01")
 
 # Leaving UTCDateTime() empty means "now":
 # and this means 24 hours ago: UTCDateTime() - 24*3600
-end = UTCDateTime("2020-02-15")
+end = UTCDateTime() - 24*3600
 
 
 # This is the time it takes to be sure the data that we download is a complete record that
@@ -21,17 +21,19 @@ safety_window = pd.Timedelta('2 days')
 
 
 network = "S1"
-station = "AUKSC"              #,sydney,brisbane jump, adelaide," # Urban stations
+station = "AUDAR"              #,sydney,brisbane jump, adelaide," # Urban stations
 location = "*"
-channel = "BHZ"
-dataset = "Australia_AUKSC"
-time_zone = "Australia/Melbourne"
-sitedesc = "Keysborough Secondary College, Victoria"
+channel = "HHZ"
+dataset = "AuSiS_Canberra"
+time_zone = "Australia/ACT"
+sitedesc = "Daramalan College, ACT"
 
 data_provider = "IRIS"
 logo = None # 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Logo_SED_2014.png/220px-Logo_SED_2014.png'
 bans = {
-        "2020-01-28 00:00":"End of School Summer Holiday"
+        "2020-01-28 00:00":"End of School Summer Holiday",
+        "2020-03-18 00:00":'No Large Gatherings',
+        "2020-03-25 12:00":'Restaurants/Bars/Schools closed',
         }
 
 
